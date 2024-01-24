e1f14567b51af35d5c30fcfe784f97e8ef8aa453
dinasoa <hei.dinasoa@gmail.com>
2024-01-24 12:41:27 +0300

    fix: wrong bucketPath

--- src/main/java/school/hei/geojobs/service/event/ZoneTilingTaskCreatedService.java
+++ src/main/java/school/hei/geojobs/service/event/ZoneTilingTaskCreatedService.java
@@ -13,15 +13,15 @@ import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.hei.geojobs.endpoint.event.gen.ZoneTilingTaskCreated;
+import school.hei.geojobs.endpoint.rest.model.TileCoordinates;
+import school.hei.geojobs.file.BucketComponent;
 import school.hei.geojobs.file.BucketConf;
 import school.hei.geojobs.model.exception.ApiException;
 import school.hei.geojobs.repository.model.Tile;
-import school.hei.geojobs.service.geo.TilesDownloader;
-import school.hei.geojobs.endpoint.rest.model.TileCoordinates;
-import school.hei.geojobs.file.BucketComponent;
 import school.hei.geojobs.repository.model.ZoneTilingTask;
 import school.hei.geojobs.repository.model.geo.Parcel;
 import school.hei.geojobs.service.ZoneTilingTaskStatusService;
+import school.hei.geojobs.service.geo.TilesDownloader;
 
 @Service
 @AllArgsConstructor
@@ -64,12 +64,25 @@ public class ZoneTilingTaskCreatedService implements Consumer<ZoneTilingTaskCrea
       var z = Integer.valueOf(dir[dir.length - 3]);
       var y = Integer.valueOf(stripExtension(tilesFile.getName()));
       String bucketName = bucketConf.getBucketName();
+      String[] segments = entryParentPath.split("/");
+      String filePath = "";
+
+      if (segments.length >= 3) {
+        filePath =
+            "/"
+                + segments[segments.length - 3]
+                + "/"
+                + segments[segments.length - 2]
+                + "/"
+                + segments[segments.length - 1];
+      }
+
       enrichedAccumulator.add(
           Tile.builder()
               .id(randomUUID().toString())
               .creationDatetime(Instant.now().toString())
               .coordinates(new TileCoordinates().x(x).y(y).z(z))
-              .bucketPath(bucketName + "/" + bucketKey + tilesFile.getPath())
+              .bucketPath(bucketName + "/" + bucketKey + filePath)
               .build());
 
       return enrichedAccumulator;
