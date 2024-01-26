1c1bf9369417df8c0b9cd14209689b758c5c5c24
dinasoa <hei.dinasoa@gmail.com>
2024-01-26 11:37:56 +0300

    chore: update tiles downloader api url

--- src/test/java/app/bpartners/geojobs/conf/EnvConf.java
+++ src/test/java/app/bpartners/geojobs/conf/EnvConf.java
@@ -7,7 +7,7 @@ public class EnvConf {
   void configureProperties(DynamicPropertyRegistry registry) {
     registry.add(
         "tiles.downloader.api.url",
-        () -> "https://hds4hpeacrrder7h32qy4lmy4a0wrewx.lambda-url.eu-west-3.on.aws");
+        () -> "https://p36gjgisjpttscydepu3suuupi0wkute.lambda-url.eu-west-3.on.aws");
     registry.add(
         "tile.detection.api.url",
         () -> "https://3gl56b3gqs6cfynuzqkte2rthm0onaoh.lambda-url.eu-west-3.on.aws");
