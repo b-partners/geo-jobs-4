cd5e77d70101cb3d5c20020a7263214bdfd1e52a
Ryan Andriamahery <ryan@hei.school>
2024-02-13 12:02:39 +0300

    chore: refactor ZoneDetectionJobStatusChangedService

--- src/main/java/app/bpartners/geojobs/service/event/ZoneDetectionJobStatusChangedService.java
+++ src/main/java/app/bpartners/geojobs/service/event/ZoneDetectionJobStatusChangedService.java
@@ -1,9 +1,10 @@
 package app.bpartners.geojobs.service.event;
 
-import static app.bpartners.geojobs.job.model.Status.ProgressionStatus.FINISHED;
-import static app.bpartners.geojobs.job.model.Status.ProgressionStatus.PROCESSING;
+import static app.bpartners.geojobs.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
 
 import app.bpartners.geojobs.endpoint.event.gen.ZoneDetectionJobStatusChanged;
+import app.bpartners.geojobs.model.exception.ApiException;
+import app.bpartners.geojobs.repository.model.detection.ZoneDetectionJob;
 import app.bpartners.geojobs.service.detection.DetectionFinishedMailer;
 import app.bpartners.geojobs.service.detection.ZoneDetectionJobService;
 import java.util.function.Consumer;
@@ -20,15 +21,41 @@ public class ZoneDetectionJobStatusChangedService
   private final ZoneDetectionJobService zoneDetectionJobService;
 
   @Override
-  public void accept(ZoneDetectionJobStatusChanged zoneDetectionJobStatusChanged) {
-    var oldJob = zoneDetectionJobStatusChanged.getOldJob();
-    var newJob = zoneDetectionJobStatusChanged.getNewJob();
-    var oldProgression = oldJob.getStatus().getProgression();
-    var newProgression = newJob.getStatus().getProgression();
+  public void accept(ZoneDetectionJobStatusChanged event) {
+    var oldJob = event.getOldJob();
+    var oldStatus = oldJob.getStatus();
+    var oldProgression = oldStatus.getProgression();
 
-    if (oldProgression == PROCESSING && newProgression == FINISHED) {
-      mailer.accept(newJob);
-      zoneDetectionJobService.handleInDoubtObjects(newJob);
+    var newJob = event.getNewJob();
+    var newStatus = newJob.getStatus();
+    var newProgression = newStatus.getProgression();
+    var newHealth = newStatus.getHealth();
+
+    if (oldStatus.equals(newStatus)) {
+      log.warn("Status did not change, yet change event received: event=" + event);
+      return;
     }
+
+    var illegalFinishedMessage = "Cannot finish as unknown, event=" + event;
+    var notFinishedMessage = "Not finished yet, nothing to do, event=" + event;
+    var doNothingMessage = "Old job already finished, do nothing";
+    var message =
+            switch (oldProgression) {
+              case PENDING, PROCESSING -> switch (newProgression) {
+                case FINISHED -> switch (newHealth) {
+                  case UNKNOWN -> throw new IllegalStateException(illegalFinishedMessage);
+                  case SUCCEEDED -> handleFinishedJob(newJob);
+                  case FAILED -> throw new ApiException(SERVER_EXCEPTION, "Failed to process zdj=" + newJob);
+                };
+                case PENDING, PROCESSING -> notFinishedMessage;
+              };
+              case FINISHED -> doNothingMessage;
+            };
+    log.info(message);
+  }
+  private String handleFinishedJob(ZoneDetectionJob zdj) {
+    mailer.accept(zdj);
+    zoneDetectionJobService.handleInDoubtObjects(zdj);
+    return "Finished, mail sent, zdj=" + zdj;
   }
 }
