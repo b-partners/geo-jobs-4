b-partners/geo-jobs

fix: use correct values in InDoubtTileDetectedService

src/main/java/app/bpartners/geojobs/service/annotator/LabelExtractor.java
@@ -1,0 +1,53 @@
+package app.bpartners.geojobs.service.annotator;
+
+import static java.util.UUID.randomUUID;
+import static java.util.stream.Collectors.toSet;
+
+import app.bpartners.geojobs.repository.annotator.gen.AnnotatedTask;
+import app.bpartners.geojobs.repository.annotator.gen.Annotation;
+import app.bpartners.geojobs.repository.annotator.gen.AnnotationBatch;
+import app.bpartners.geojobs.repository.annotator.gen.Label;
+import app.bpartners.geojobs.repository.model.detection.DetectableObjectType;
+import java.util.HashSet;
+import java.util.List;
+import java.util.function.Function;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LabelExtractor implements Function<DetectableObjectType.DetectableType, Label> {
+  @Override
+  public Label apply(DetectableObjectType.DetectableType detectableType) {
+    return Label.builder()
+        .id(randomUUID().toString())
+        .color(getColorFromDetectedType(detectableType))
+        .name(detectableType.name())
+        .build();
+  }
+
+  private static String getColorFromDetectedType(
+      DetectableObjectType.DetectableType detectableType) {
+    return switch (detectableType) {
+      case ROOF -> "#DFFF00";
+      case SOLAR_PANEL -> "#0E4EB3";
+      case POOL -> "#0DCBD2";
+      case PATHWAY -> "#F5F586";
+      case TREE -> "#4BFF33";
+      default -> throw new IllegalArgumentException("unexpected value");
+    };
+  }
+
+  public List<Label> extractLabelsFromTasks(List<AnnotatedTask> annotatedTasks) {
+    return annotatedTasks.stream()
+        .map(AnnotatedTask::getAnnotationBatch)
+        .map(AnnotationBatch::getAnnotations)
+        .map(a -> a.stream().map(Annotation::getLabel).collect(toSet()))
+        .reduce(
+            new HashSet<>(),
+            (acc, val) -> {
+              acc.addAll(val);
+              return acc;
+            })
+        .stream()
+        .toList();
+  }
+}

b-partners/geo-jobs-4

src/main/java/app/bpartners/geojobs/service/annotator/LabelExtractor.java
@@ -1,0 +1,52 @@
+package app.bpartners.geojobs.service.annotator;
+
+import static java.util.UUID.randomUUID;
+import static java.util.stream.Collectors.toSet;
+
+import app.bpartners.annotator.endpoint.rest.model.AnnotatedTask;
+import app.bpartners.annotator.endpoint.rest.model.Annotation;
+import app.bpartners.annotator.endpoint.rest.model.AnnotationBatch;
+import app.bpartners.annotator.endpoint.rest.model.Label;
+import app.bpartners.geojobs.repository.model.detection.DetectableObjectType;
+import java.util.HashSet;
+import java.util.List;
+import java.util.function.Function;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LabelExtractor implements Function<DetectableObjectType.DetectableType, Label> {
+  @Override
+  public Label apply(DetectableObjectType.DetectableType detectableType) {
+    return new Label()
+        .id(randomUUID().toString())
+        .color(getColorFromDetectedType(detectableType))
+        .name(detectableType.name());
+  }
+
+  private static String getColorFromDetectedType(
+      DetectableObjectType.DetectableType detectableType) {
+    return switch (detectableType) {
+      case ROOF -> "#DFFF00";
+      case SOLAR_PANEL -> "#0E4EB3";
+      case POOL -> "#0DCBD2";
+      case PATHWAY -> "#F5F586";
+      case TREE -> "#4BFF33";
+      default -> throw new IllegalArgumentException("unexpected value");
+    };
+  }
+
+  public List<Label> extractLabelsFromTasks(List<AnnotatedTask> annotatedTasks) {
+    return annotatedTasks.stream()
+        .map(AnnotatedTask::getAnnotationBatch)
+        .map(AnnotationBatch::getAnnotations)
+        .map(a -> a.stream().map(Annotation::getLabel).collect(toSet()))
+        .reduce(
+            new HashSet<>(),
+            (acc, val) -> {
+              acc.addAll(val);
+              return acc;
+            })
+        .stream()
+        .toList();
+  }
+}