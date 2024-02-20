b-partners/geo-jobs

chore(to-complete): handle indoubt tiles detected event inside its own service

src/main/java/app/bpartners/geojobs/service/event/InDoubtTileDetectedService.java
@@ -1,7 +1,22 @@
 package app.bpartners.geojobs.service.event;
 
+import static java.util.UUID.randomUUID;
+
 import app.bpartners.geojobs.endpoint.event.gen.InDoubtTilesDetected;
-import app.bpartners.geojobs.model.exception.NotImplementedException;
+import app.bpartners.geojobs.repository.DetectedTileRepository;
+import app.bpartners.geojobs.repository.annotator.AnnotatorApiClient;
+import app.bpartners.geojobs.repository.annotator.gen.AnnotatedTask;
+import app.bpartners.geojobs.repository.annotator.gen.Annotation;
+import app.bpartners.geojobs.repository.annotator.gen.AnnotationBatch;
+import app.bpartners.geojobs.repository.annotator.gen.CrupdateAnnotatedJob;
+import app.bpartners.geojobs.repository.annotator.gen.JobStatus;
+import app.bpartners.geojobs.repository.annotator.gen.Label;
+import app.bpartners.geojobs.repository.annotator.gen.Point;
+import app.bpartners.geojobs.repository.annotator.gen.Polygon;
+import app.bpartners.geojobs.repository.model.detection.DetectedObject;
+import app.bpartners.geojobs.repository.model.detection.DetectedTile;
+import java.time.Instant;
+import java.util.List;
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,8 +24,63 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class InDoubtTileDetectedService implements Consumer<InDoubtTilesDetected> {
+  private final DetectedTileRepository detectedTileRepository;
+  private final AnnotatorApiClient annotatorApiClient;
+
   @Override
-  public void accept(InDoubtTilesDetected inDoubtTilesDetected) {
-    throw new NotImplementedException("Not supported : " + inDoubtTilesDetected);
+  public void accept(InDoubtTilesDetected event) {
+    String jobId = event.getJobId();
+    Instant updatedAt = Instant.now();
+    List<DetectedTile> detectedTiles = detectedTileRepository.findAllByJobId(jobId);
+    List<DetectedTile> detectedInDoubtTiles =
+        detectedTiles.stream()
+            .filter(
+                detectedTile ->
+                    detectedTile.getDetectedObjects().stream().anyMatch(DetectedObject::isInDoubt))
+            .toList();
+    // /!\ TODO: complete TODO by converting detected in-doubt tiles
+
+    Label label = Label.builder().id(randomUUID().toString()).name("TODO").color("TODO").build();
+
+    annotatorApiClient.crupdateAnnotatedJob(
+        CrupdateAnnotatedJob.builder()
+            .id(randomUUID().toString())
+            .name("TODO")
+            .bucketName("TODO")
+            .folderPath("TODO")
+            .labels(List.of(label))
+            .ownerEmail(null)
+            .status(JobStatus.TO_REVIEW)
+            .annotatedTasks(
+                List.of(
+                    AnnotatedTask.builder()
+                        .id(randomUUID().toString())
+                        .annotatorId("TODO")
+                        .filename("TODO")
+                        .annotationBatch(
+                            AnnotationBatch.builder()
+                                .id(randomUUID().toString())
+                                .creationDatetime(updatedAt)
+                                .annotations(
+                                    List.of(
+                                        Annotation.builder()
+                                            .id(randomUUID().toString())
+                                            .userId("TODO")
+                                            .taskId("TODO")
+                                            .label(label)
+                                            .polygon(
+                                                Polygon.builder()
+                                                    .points(
+                                                        List.of(
+                                                            Point.builder()
+                                                                .y(0.0) // TODO
+                                                                .y(0.0) // TODO
+                                                                .build()))
+                                                    .build())
+                                            .build()))
+                                .build())
+                        .build()))
+            .teamId("TODO")
+            .build());
   }
 }

b-partners/geo-jobs-4

src/main/java/app/bpartners/geojobs/service/event/InDoubtTileDetectedService.java
@@ -1,16 +1,94 @@
 package app.bpartners.geojobs.service.event;
 
+import static app.bpartners.annotator.endpoint.rest.model.JobStatus.TO_REVIEW;
+import static app.bpartners.geojobs.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
+import static java.util.UUID.randomUUID;
+
+import app.bpartners.annotator.endpoint.rest.api.AnnotatedJobsApi;
+import app.bpartners.annotator.endpoint.rest.model.AnnotatedTask;
+import app.bpartners.annotator.endpoint.rest.model.Annotation;
+import app.bpartners.annotator.endpoint.rest.model.AnnotationBatch;
+import app.bpartners.annotator.endpoint.rest.model.CrupdateAnnotatedJob;
+import app.bpartners.annotator.endpoint.rest.model.Label;
+import app.bpartners.annotator.endpoint.rest.model.Point;
+import app.bpartners.annotator.endpoint.rest.model.Polygon;
 import app.bpartners.geojobs.endpoint.event.gen.InDoubtTilesDetected;
-import app.bpartners.geojobs.model.exception.NotImplementedException;
+import app.bpartners.geojobs.model.exception.ApiException;
+import app.bpartners.geojobs.repository.DetectedTileRepository;
+import app.bpartners.geojobs.repository.model.detection.DetectedObject;
+import app.bpartners.geojobs.repository.model.detection.DetectedTile;
+import app.bpartners.geojobs.service.annotator.AnnotatorApiConf;
+import java.time.Instant;
+import java.util.List;
 import java.util.function.Consumer;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 public class InDoubtTileDetectedService implements Consumer<InDoubtTilesDetected> {
+  private final DetectedTileRepository detectedTileRepository;
+  private final AnnotatedJobsApi annotatedJobsApi;
+
+  public InDoubtTileDetectedService(
+      DetectedTileRepository detectedTileRepository, AnnotatorApiConf annotatorApiConf) {
+    this.detectedTileRepository = detectedTileRepository;
+    this.annotatedJobsApi = new AnnotatedJobsApi(annotatorApiConf.newApiClientWithApiKey());
+  }
+
   @Override
-  public void accept(InDoubtTilesDetected inDoubtTilesDetected) {
-    throw new NotImplementedException("Not supported : " + inDoubtTilesDetected);
+  public void accept(InDoubtTilesDetected event) {
+    String jobId = event.getJobId();
+    Instant updatedAt = Instant.now();
+    List<DetectedTile> detectedTiles = detectedTileRepository.findAllByJobId(jobId);
+    List<DetectedTile> detectedInDoubtTiles =
+        detectedTiles.stream()
+            .filter(
+                detectedTile ->
+                    detectedTile.getDetectedObjects().stream().anyMatch(DetectedObject::isInDoubt))
+            .toList();
+    // /!\ TODO: complete TODO by converting detected in-doubt tiles
+
+    Label label = new Label().id(randomUUID().toString()).name("TODO").color("TODO");
+
+    String crupdateJobId = randomUUID().toString();
+    try {
+      annotatedJobsApi.crupdateAnnotatedJob(
+          crupdateJobId,
+          new CrupdateAnnotatedJob()
+              .id(crupdateJobId)
+              .name("TODO")
+              .bucketName("TODO")
+              .folderPath("TODO")
+              .labels(List.of(label))
+              .ownerEmail(null)
+              .status(TO_REVIEW)
+              .annotatedTasks(
+                  List.of(
+                      new AnnotatedTask()
+                          .id(randomUUID().toString())
+                          .annotatorId("TODO")
+                          .filename("TODO")
+                          .annotationBatch(
+                              new AnnotationBatch()
+                                  .id(randomUUID().toString())
+                                  .creationDatetime(updatedAt)
+                                  .annotations(
+                                      List.of(
+                                          new Annotation()
+                                              .id(randomUUID().toString())
+                                              .userId("TODO")
+                                              .taskId("TODO")
+                                              .label(label)
+                                              .polygon(
+                                                  new Polygon()
+                                                      .points(
+                                                          List.of(
+                                                              new Point()
+                                                                  .y(0.0) // TODO
+                                                                  .y(0.0) // TODO
+                                                              ))))))))
+              .teamId("TODO"));
+    } catch (app.bpartners.annotator.endpoint.rest.client.ApiException e) {
+      throw new ApiException(SERVER_EXCEPTION, e);
+    }
   }
 }