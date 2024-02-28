7e8024d3bebb678c5a79f494d67530f647a98953
Lou Andria <lou@hei.school>
2024-02-28 19:57:17 +0100

    fix: hikari conf cannot be read from properties when deployed in lambda

--- src/test/java/app/bpartners/geojobs/service/annotator/ExtractorIT.java
+++ src/test/java/app/bpartners/geojobs/service/annotator/ExtractorIT.java
@@ -33,18 +33,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class ExtractorIT extends FacadeIT {
   private static final String MOCK_FEATURE_AS_STRING =
       """
-            { "type": "Feature",
-              "properties": {
-                "code": "69",
-                "nom": "Rhône",
-                "id": 30251921,
-                "CLUSTER_ID": 99520,
-                "CLUSTER_SIZE": 386884 },
-              "geometry": {
-                "type": "MultiPolygon",
-                "coordinates": [ [ [
-                  [ 4.459648282829194, 45.904988912620688 ]
-                  ] ] ] } }""";
+          { "type": "Feature",
+            "properties": {
+              "code": "69",
+              "nom": "Rhône",
+              "id": 30251921,
+              "CLUSTER_ID": 99520,
+              "CLUSTER_SIZE": 386884 },
+            "geometry": {
+              "type": "MultiPolygon",
+              "coordinates": [ [ [
+                [ 4.459648282829194, 45.904988912620688 ]
+                ] ] ] } }""";
   public static final String PARCEL_MOCK_ID = "parcel1";
   @Autowired private ObjectMapper om;
   @Autowired private LabelExtractor labelExtractor;
