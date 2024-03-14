fc032a139604e9f05c27353717b0b2cb3799b953
Mahefa Ny Anjara <98638690+Mahefaa@users.noreply.github.com>
2024-03-14 17:30:40 +0300

    fix(to-revert): revert workers

--- src/main/java/app/bpartners/geojobs/concurrency/Workers.java
+++ src/main/java/app/bpartners/geojobs/concurrency/Workers.java
@@ -1,9 +1,6 @@
 package app.bpartners.geojobs.concurrency;
 
-import static app.bpartners.geojobs.concurrency.ThreadRenamer.getRandomSubThreadNamePrefixFrom;
-import static app.bpartners.geojobs.concurrency.ThreadRenamer.renameThread;
-import static java.lang.Thread.currentThread;
-import static java.util.concurrent.Executors.newVirtualThreadPerTaskExecutor;
+import static java.util.concurrent.Executors.newFixedThreadPool;
 
 import app.bpartners.geojobs.PojaGenerated;
 import java.util.List;
@@ -11,6 +8,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @PojaGenerated
@@ -18,24 +16,12 @@ import org.springframework.stereotype.Component;
 public class Workers<T> {
   private final ExecutorService executorService;
 
-  public Workers() {
-    this.executorService = newVirtualThreadPerTaskExecutor();
+  public Workers(@Value("${workers.thread.number:5}") int nbThreads) {
+    this.executorService = newFixedThreadPool(nbThreads);
   }
 
   @SneakyThrows
   public List<Future<T>> invokeAll(List<Callable<T>> callables) {
-    var parentThread = currentThread();
-    callables =
-        callables.stream()
-            .map(
-                c ->
-                    (Callable<T>)
-                        () -> {
-                          renameThread(
-                              currentThread(), getRandomSubThreadNamePrefixFrom(parentThread));
-                          return c.call();
-                        })
-            .toList();
     return executorService.invokeAll(callables);
   }
 }
