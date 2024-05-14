e7ed6bcf033d9d5c07c3005eab9c2d9e9466dbbb
dinasoa <hei.dinasoa@gmail.com>
2024-05-14 15:30:49 +0300

    chore: set worker_reserved_concurrent_executions_nb to 250

--- src/test/java/app/bpartners/geojobs/conf/FacadeIT.java
+++ src/test/java/app/bpartners/geojobs/conf/FacadeIT.java
@@ -10,11 +10,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @PojaGenerated
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@EnableTransactionManagement
 @Slf4j
 public class FacadeIT {
 
