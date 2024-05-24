ec4b1fde348c3f8525859ad362c664a0e6be5020
Ryan Andriamahery <ryan@hei.school>
2024-05-24 14:04:26 +0300

    fix: avoid NPE when converting import ZTJ parameters

--- src/main/java/app/bpartners/geojobs/endpoint/rest/controller/ZoneTilingController.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/controller/ZoneTilingController.java
@@ -42,8 +42,12 @@ public class ZoneTilingController {
     var bucketPathPrefix = importZoneTilingJob.getBucketPathPrefix();
     var geoServerParameter = importZoneTilingJob.getCreateZoneTilingJob().getGeoServerParameter();
     var geoServerUrl = importZoneTilingJob.getCreateZoneTilingJob().getGeoServerUrl();
-    var startFrom = importZoneTilingJob.getStartFrom().longValue();
-    var endAt = importZoneTilingJob.getEndAt().longValue();
+    var startFrom =
+        importZoneTilingJob.getStartFrom() == null
+            ? null
+            : importZoneTilingJob.getStartFrom().longValue();
+    var endAt =
+        importZoneTilingJob.getEndAt() == null ? null : importZoneTilingJob.getEndAt().longValue();
     return mapper.toRest(
         service.importFromBucket(
             job, bucketName, bucketPathPrefix, geoServerParameter, geoServerUrl, startFrom, endAt),
