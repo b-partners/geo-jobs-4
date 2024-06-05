665544a2b7993f3f0b7234bb07512a69ce2783a1
Ryan Andriamahery <ryan@hei.school>
2024-06-05 13:36:32 +0300

    chore: test pathway detection api from aws

--- src/test/java/app/bpartners/geojobs/conf/EnvConf.java
+++ src/test/java/app/bpartners/geojobs/conf/EnvConf.java
@@ -18,15 +18,16 @@ public class EnvConf {
         () ->
             "[ { \"objectType\": \"ROOF\", \"url\": \"https://roof-api.azurewebsites.net/api\" }, {"
                 + " \"objectType\": \"PATHWAY\", \"url\":"
-                + " \"https://pathway-api.azurewebsites.net/api\" }, { \"objectType\":"
-                + " \"SOLAR_PANEL\", \"url\": \"https://solarpanel-api.azurewebsites.net/api\" }, {"
-                + " \"objectType\": \"POOL\", \"url\": \"https://pool-api.azurewebsites.net/api\""
-                + " }, { \"objectType\": \"TREE\", \"url\":"
-                + " \"https://trees-api.azurewebsites.net/api\" }, { \"objectType\": \"SIDEWALK\","
-                + " \"url\": \"https://sidewalk-api.azurewebsites.net/api\" }, { \"objectType\":"
-                + " \"LINE\", \"url\": \"https://line-api.azurewebsites.net/api\" }, {"
-                + " \"objectType\": \"GREEN_SPACE\", \"url\":"
-                + " \"https://greenspace-api.azurewebsites.net/api\" } ]");
+                + " \"https://2p3jsxl3zadpi7r5fysmq4mzsu0fzphw.lambda-url.eu-west-3.on.aws\" }, {"
+                + " \"objectType\": \"SOLAR_PANEL\", \"url\":"
+                + " \"https://solarpanel-api.azurewebsites.net/api\" }, { \"objectType\": \"POOL\","
+                + " \"url\": \"https://pool-api.azurewebsites.net/api\" }, { \"objectType\":"
+                + " \"TREE\", \"url\": \"https://trees-api.azurewebsites.net/api\" }, {"
+                + " \"objectType\": \"SIDEWALK\", \"url\":"
+                + " \"https://sidewalk-api.azurewebsites.net/api\" }, { \"objectType\": \"LINE\","
+                + " \"url\": \"https://line-api.azurewebsites.net/api\" }, { \"objectType\":"
+                + " \"GREEN_SPACE\", \"url\": \"https://greenspace-api.azurewebsites.net/api\" }"
+                + " ]");
     registry.add("admin.api.key", () -> "the-admin-api-key");
     registry.add("annotator.api.key", () -> "the-admin-api-key");
     registry.add(
